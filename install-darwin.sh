8fa9bd492befc1875c62899da2733986172e65d8
Jarrod Overson <jsoverson@gmail.com>
2023-11-24 22:07:08 -0500

    updated darwin install

--- install-darwin.sh
+++ install-darwin.sh
@@ -2,57 +2,71 @@
 
 set -e
 
-# oh-my-zsh
+function exists {
+  type $1 2>/dev/null >/dev/null
+}
 
+function brew_install {
+  if ! exists $1; then
+    brew install $1
+  fi
+}
+
+function app_store_install {
+  if ! exists $1; then
+    mas install $2
+  fi
+}
+
+# oh-my-zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
   echo "Installing oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Homebrew
-if ! type "brew" >/dev/null; then
+if ! exists "brew"; then
   echo "Installing homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  brew install jq mas coreutils
+  brew install jq mas coreutils starship
 fi
+
+brew_install jq
+brew_install mas
+brew_install coreutils
+brew_install starship
+
+# /End homebrew
 
 # /Begin app store
-
-#slack
-if ! type "/Applications/Slack.app/Contents/MacOS/Slack" >/dev/null; then
-  mas install 803453959
-fi
-
-#keynote
-if ! type "/Applications/Keynote.app/Contents/MacOS/Keynote" >/dev/null; then
-  mas install 409183694
-fi
-
+app_store_install "/Applications/Slack.app/Contents/MacOS/Slack" 803453959
+app_store_install "/Applications/Keynote.app/Contents/MacOS/Keynote" 409183694
 # /End app store
 
 #1password
-if ! type "/Applications/1Password.app/Contents/MacOS/1Password" >/dev/null; then
+if ! exists "/Applications/1Password.app/Contents/MacOS/1Password"; then
   curl -L https://downloads.1password.com/mac/1Password.zip -o /tmp/1password.zip
   unzip /tmp/1password.zip -d /tmp/1password
   open /tmp/1password/1Password\ Installer.app/
 fi
 
 #iterm2
-if ! type "/Applications/iTerm.app/Contents/MacOS/iTerm2" >/dev/null; then
+if ! exists "/Applications/iTerm.app/Contents/MacOS/iTerm2"; then
   curl -L "https://iterm2.com/downloads/stable/latest" -o /tmp/iterm.zip
   unzip /tmp/iterm.zip -d /Applications
 fi
 
 #vscode
-if ! type "/Applications/Visual Studio Code.app/Contents/MacOS/Electron" >/dev/null; then
+if ! exists "/Applications/Visual Studio Code.app/Contents/MacOS/Electron"; then
   curl -L "https://go.microsoft.com/fwlink/?LinkID=620882" -o /tmp/vscode.zip
   unzip /tmp/vscode.zip -d /Applications
 fi
 
-if ! type "/Applications/Alfred.app/Contents/MacOS/Alfred" >/dev/null; then
+if ! exists "/Applications/Alfred.app/Contents/MacOS/Alfred"; then
   curl -L "https://cachefly.alfredapp.com/Alfred_5.1.4_2195.dmg" -o /tmp/alfred.dmg
   hdiutil mount /tmp/alfred.dmg
+  cp -r /Volumes/Alfred/Alfred\ 5.app /Applications
 fi
 
 ./osx-preferences.sh
